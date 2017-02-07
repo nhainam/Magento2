8ef183b844b268c7d6637e2a2cc8450a5bf957f0
nhainam <nhainam@tma.com.vn>
2017-02-07 17:09:32 +0700

    Orm module

---
+++ app/code/Training/Orm/Setup/UpgradeData.php
@@ -0,0 +1,16 @@
+<?php
+namespace Training\Unit4\Setup;
+
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class UpgradeData implements UpgradeDataInterface
+{
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        if (version_compare($context->getVersion(), '2.0.1', '<')) {
+
+        }
+    }
+}
